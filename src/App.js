d9b04db000de8a7e601e24bfaaa98b8b9a715693
Palak Mishra <palakmishra170101@gmail.com>
2024-12-17 23:48:14 +0530

    LandingPage, SignupPage, LoginPage, RecoveryPage, ProfilePage

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import "./styles/index.css";
+
+const App = () => {
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+};
+
+console.log('App is rendering');
+
+
+export default App;
