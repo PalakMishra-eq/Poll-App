d6a69a32c42bbac62ac87ce2fa7f7c1726efac3f
Palak Mishra <54719626+PalakMishra-eq@users.noreply.github.com>
2024-12-29 14:49:39 +0530

    Update pollRoutes.js

--- routes/pollRoutes.js
+++ routes/pollRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { auth, roleAuthorization } = require('../middlewares/authMiddleware');
 const { checkPollStatus } = require('../middlewares/pollMiddleware');
-const { createPoll, voteOnPoll, deletePoll, getPollResults, searchPolls, getUserVotes, reportPoll } = require('../controllers/pollController');
+const { createPoll, voteOnPoll, deletePoll, getPollResults, searchPolls, getUserVotes, reportPoll, getPollDetails } = require('../controllers/pollController');
 const rateLimiter = require('../middlewares/rateLimiterMiddleware');
 
 // Route for admins to create a poll
@@ -27,6 +27,9 @@ router.get('/polls', searchPolls);
 
 // Route to get polls voted by the user
 router.get('/my-votes', auth, roleAuthorization(['Voter']), getUserVotes);
+
+// Route to get poll details by ID
+router.get('/:pollId/details', auth, getPollDetails);
 
 // Search and filter polls
 router.get('/', searchPolls);
